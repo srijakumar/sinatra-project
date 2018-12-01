15e7ba5aeb901bb0244444d3f5e20e520f2b8a63
Srija S <srijakumar7@gmail.com>
2018-12-01 08:30:42 +0000

    skeleton request controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,3 +1,40 @@
 class RequestsController < ApplicationController
 
+get '/request' do
+  if logged_in?
+    @requests = Request.all
+    erb :'requests/requests'
+  else
+    redirect to '/login'
+end
+
+get '/requests/new' do
+  if logged_in?
+    erb :'requests/create_request'
+  else
+    redirect to '/login'
+  end
+end
+
+post '/requests' do
+
+  #binding.pry
+  if logged_in?
+    if params["content"] == ""
+      redirect '/requests/new'
+    else
+
+    @request=Request.new(:content => params["content"], :tenant_id => current_tenant.id, :apt_num => params["apt_num"], :date => params["date"])
+
+    if @request.save
+     redirect to "/requests/#{@request.id}"
+    else
+      redirect to '/requests/new'
+    end
+
+  end
+  redirect to '/login'
+end
+end
+
 end
