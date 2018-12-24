6819369abae3677615710ed2d2f5fb46aa0101f8
Srija S <srijakumar7@gmail.com>
2018-12-24 17:10:53 +0000

    Updated email to string type

--- db/migrate/01_create_tenants_table.rb
+++ db/migrate/01_create_tenants_table.rb
@@ -2,7 +2,7 @@ class CreateTenantsTable < ActiveRecord::Migration
   def change
     create_table :tenants do |t|
       t.string :username
-      t.text :email
+      t.string :email
       t.string :password_digest
 
       t.timestamps null: false
